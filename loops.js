3fc80a13684f4e4503f6ef9bd33665bca83b8a90
Eliana K <ekirshe@gmail.com>
2018-07-20 18:48:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,12 +2,14 @@ function forLoop(array){
 
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push("I am ${i} strange loop.")}
-    else {
-      array.push("I am ${i} strange loops.")}
+      array.push("I am ${i} strange loop.")
+    }
+      else {
+        array.push(`I am ${i} strange loops.`)
+      }
         return array;
   }
-}
+};
 
 function whileLoop(n){
   while(whileLoop(n < 2)){
